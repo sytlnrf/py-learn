639bc319efc81e8238796ce5607aa62cb384389c
sytlnrf <sytlnrf@gmail.com>
2019-03-18 18:22:15 +0800

    py2.7 yo py3.5

--- src/test/basic.py
+++ src/test/basic.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ### tube
 tu = (1, 2, 3, 4, 1)
-#print tu.index(5)
-print tu.count(1)
-print tu == 1
-print tu + tu
-print tu * 4
+print(tu.index(4))
+print(tu.count(1))
+print(tu == 1)
+print(tu + tu)
+print(tu * 4)
